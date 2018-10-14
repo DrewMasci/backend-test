8a65175ae26bff42d09267e629ff2485681c2c9f
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 18:49:37 +0100

    additions to the unit testing

--- tests/Unit/CachedFeedDataTest.php
+++ tests/Unit/CachedFeedDataTest.php
@@ -10,7 +10,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class AllArray extends TestCase
 {
     /**
-     * A basic test example.
+     * tests the returns of the allArray function from cachedFeedData is
+     * returning an array, that the status code is correctly set, and that it is
+     * returning 200.
      *
      * @return void
      */
@@ -34,7 +36,8 @@ class AllArray extends TestCase
     }
 
     /**
-     * A basic test example.
+     * tests the arrayGlue function of cachedFeedData to make sure that the
+     * arrays are combined correctly.
      *
      * @return void
      */
@@ -86,6 +89,47 @@ class AllArray extends TestCase
         ];
 
         $r = cachedFeedData::arrayGlue($array1, $array2);
+
+        if($r != $arrayR) {
+            $this->assertTrue(false);
+        }
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Tests the sanitiseArray function from cachedFeedData to make sure all
+     * null values are removed from the given array.
+     *
+     * @return void
+     */
+    public function testSanitiseArray()
+    {
+        $array1 = [
+            'city' => [
+                'location' => [
+                    'provider' => 'test provider 1',
+                    'version' => null,
+                ],
+                'locations' => [
+                    'name' => 'City 1',
+                    'country' => null,
+                ]
+            ]
+        ];
+
+        $arrayR = [
+            'city' => [
+                'location' => [
+                    'provider' => 'test provider 1',
+                ],
+                'locations' => [
+                    'name' => 'City 1',
+                ]
+            ]
+        ];
+
+        $r = cachedFeedData::sanitiseArray($array1);
 
         if($r != $arrayR) {
             $this->assertTrue(false);
